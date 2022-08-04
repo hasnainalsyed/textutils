2ddc4b90816da243f4bd8e4a9362233b1abfcec8
Hasnain Syed <hasnainalsyed@gmail.com>
2022-08-04 16:40:59 +0500

    First Exercies Done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,20 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
 
   const handleUpClick = () => {
-    let upperCaseText = text.toUpperCase();
-    setText(upperCaseText);
+    setText(text.toUpperCase());
   }
+
   const handleLowClick = () => {
-    let lowerCaseText = text.toLowerCase();
-    setText(lowerCaseText);
+    setText(text.toLowerCase());
   }
+
+  const handleClearClick = () => {
+    setText('');
+  }
+
+  const handleCapClick = () => {
+    const separateWord = text.toLowerCase().split(' ');
+    for (var i = 0; i < separateWord.length; i++) {
+      separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+      separateWord[i].substring(1);
+    }
+    const newStr = separateWord.join(' ');
+    setText(newStr);
+  }
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   }
-  const [text, setText] = useState('Enter text here..!');
+
+  const [text, setText] = useState('');
 
   return (
     <div className='container-xl mt-5'>
@@ -23,8 +38,21 @@ export default function TextForm(props) {
         <div className="mb-4">
           <textarea className="form-control rounded-3" value={text} onChange={handleOnChange} id="textArea" rows="7"></textarea>
         </div>
-        <button className="btn btn-primary me-3" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary" onClick={handleLowClick}>Convert to lowercase</button>
+        <button className="btn btn-primary me-3" onClick={handleUpClick}>CONVERT TO UPPERCASE</button>
+        <button className="btn btn-primary me-3" onClick={handleLowClick}>convert to lowercase</button>
+        <button className="btn btn-primary me-3" onClick={handleCapClick}>Captlize All</button>
+        <button className="btn btn-danger" onClick={handleClearClick}>Clear..!</button>
+      </div>
+
+      <div className="wrapper my-4">
+        <h3>Your text summery</h3>
+        <p className='fs-6 fst-italic text-success lh-sm mb-2'>{text.split(' ').length} words and {text.length} characters</p>
+        <p className='fs-6 fst-italic text-success lh-sm'>{0.008 * text.split(' ').length} Minutes to read</p>
+      </div>
+
+      <div className="wrapper my-4">
+        <h3>Preview</h3>
+        <p className='fs-6 fst-italic text-success lh-sm'>{text}</p>
       </div>
     </div>
   )
